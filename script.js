9d815edc859e8de27ee7aeec21ffde6196099578
Fukumaru <simon.c.arango.co@gmail.com>
2025-08-03 15:19:27 -0500

    added buttons, score and restart game

--- script.js
+++ script.js
@@ -1,91 +1,100 @@
-console.log("Hello World") 
-/// Generate a number between 0 and 100
-/// If number is lesser or equal than 33 print scissors
-/// If number is greater or equal than 66 print paper
-/// Else print rock
-function getComputerChoice(choice) {
-    choice = Math.floor(Math.random() * 100);
-    if (choice > 33 && choice < 66) {
-        return "Rock";
-    }
-    else if (choice <= 33) {
-    return "Scissors";
-    }
-    else (choice >= 66); {
-        return "Paper";
-    }
-    
-    }
-
-/// Create a prompt box that only receives 'Paper, Rock or scissors'
-function getHumanChoice() {
-    humanChoice = prompt("Rock, Paper or Scissors? ", ).toLowerCase();
-    if (humanChoice === "scissors")
-        return "Scissors";
-    else if (humanChoice === "rock")
-        return "Rock";
-    else if (humanChoice === "paper")
-        return "Paper";
-    else (humanChoice)
-        alert("Wrong value");
-}
-/// Create players score
-let replayButton;
 let guessCount = 0;
 let humanScore = 0;
 let computerScore = 0;
-function playGame() {
-/// Create a value for each round, then each time game is played, add 1 to value each round
 
-    rounds = 5;
-for (i = 1; i <= rounds; i++) {
-//    console.log(i);
-//}
+const selection = document.querySelector('#selection');
 
+function endGame() {
+    if (humanScore === 5 || computerScore === 5) {
+        alert("Game has ended!");
+    selection.style.display = 'none';   
+};
 
+};
+                        /// Create players score
+ const div = document.createElement("div");
+            document.body.appendChild(div);
+            let jugador = document.createElement("p");
+            jugador.id = "jugador";
+            
+            div.appendChild(jugador);
+            let maquina = document.createElement("p");
+            maquina.id = "maquina";
+            
+            div.appendChild(maquina);
+console.log("Hello World") 
 
-
-/// Create round logic
-/// If human and Computer same answer, TIE
-/// If human Paper and Computer Scissors, Scissors beats Paper
-/// If human Paper and Computer Rock, Paper beats Rock
-/// If human Rock and Computer Scissors, Rock beats Scissors
-/// If human Rock and Computer Paper, Paper beats Rock
-/// If human Paper and Computer Scissors, Scissors beats Paper
-/// If human Paper and Computer Rock, Paper beats Rock
+function getComputerChoice(choice) {
+    choice = Math.floor(Math.random() * 100);
+    if (choice > 33 && choice < 66) {
+        return "rock";
+    }
+    else if (choice <= 33) {
+        return "scissors";
+    }
+    else  {
+        return "paper";
+    }
+    
+}
 function playRound(humanChoice, choice) {
     
     if (humanChoice === choice) {
         console.log("It's a Tie!"); }
-    else if (humanChoice === "Scissors" && choice === "Rock" || humanChoice === "Rock" && choice === "Paper" || humanChoice === "Paper" && choice === "Scissors") {
-        console.log("You Lose, " + computerSelection + " Beats " + humanSelection)
-        computerScore++; }
-    else if (humanChoice === "Paper" && choice === "Rock" || humanChoice === "Rock" && choice === "Scissors" || humanChoice === "Scissors" && choice === "Paper") {
-        console.log("You Win, " + humanSelection + " Beats " + computerSelection)
-        humanScore++;  }  
-} guessCount++;
+        else if (humanChoice === "scissors" && choice === "rock" || humanChoice === "rock" && choice === "paper" || humanChoice === "paper" && choice === "scissors") {
+            computerScore++; 
+            maquina.textContent = "Machine score is = " + computerScore; }
+            else if (humanChoice === "paper" && choice === "rock" || humanChoice === "rock" && choice === "scissors" || humanChoice === "scissors" && choice === "paper") {
+                console.log("You Win, " + humanChoice + " Beats " + choice)
+                humanScore++;  
+            jugador.textContent = "Human score is = " + humanScore;}
+                console.log(humanChoice);
+                console.log(choice);
+                } 
+                jugador.textContent = "Human score is = " + humanScore;
+                maquina.textContent = "Machine score is = " + computerScore;
+            
+            /// Create a prompt box that only receives 'Paper, Rock or scissors'
+            function playGame() {
+                let replayButton;
+                replayButton = document.createElement('button');
+                replayButton.textContent = 'Start new game';
+                document.body.appendChild(replayButton);
+                replayButton.addEventListener('click', playAgain);
+                function playAgain() {    
+                    guessCount = 0;
+                    humanScore = 0;
+                    computerScore = 0;
+                    console.clear();
+                    replayButton.remove();
+                    playGame();
+                    jugador.textContent = "Human score is = " + humanScore;
+                    maquina.textContent = "Machine score is = " + computerScore;
+                    selection.style.display = ''; 
+                }
+                
+            };
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-console.log("-----------");
-console.log("Computer choose " + computerSelection)
+           
 
-playRound(humanSelection, computerSelection);
 
-console.log("Ronda #" + guessCount);
-console.log(humanScore + " Your score");
-console.log(computerScore + " Computer score");
-
-} }
-playGame(); 
-replayButton = document.createElement('button');
-replayButton.textContent = 'Start new game';
-document.body.appendChild(replayButton);
-replayButton.addEventListener('click', playAgain);
-function playAgain() {    
-    guessCount = 0;
-    humanScore = 0;
-    computerScore = 0;
-    console.clear();
-    playGame();
-}
+            playGame();
+            let humanChoice = "";
+            selection.addEventListener('click', (event) => {
+                let choice = getComputerChoice();
+                const opcion = event.target.id;
+                switch(opcion) {
+                    case 'rock':
+                        humanChoice = "rock";
+                        break;
+                        case 'paper':
+                            humanChoice = "paper";
+                            break;
+                            case 'scissors':
+                                humanChoice = "scissors";                                               
+                                break;
+                            }
+                            playRound(humanChoice, choice);
+                            endGame(humanScore, computerScore);
+                        });
+            
